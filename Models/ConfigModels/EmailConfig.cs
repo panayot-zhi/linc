ac8849f55f2a1fcc70cecbf34daf639b5b14886c
Panayot Ivanov <panayot_zhi@abv.bg>
2023-11-20 14:31:07 +0200

    email settings model

---
+++ Models/ConfigModels/EmailConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace linc.Models.ConfigModels
+{
+    public class EmailConfig
+    {
+        public int Port { get; init; }
+
+        public string Host { get; init; }
+
+        public string Username { get; init; }
+
+        public string Password { get; init; }
+
+        public bool Debug { get; init; }
+    }
+}
