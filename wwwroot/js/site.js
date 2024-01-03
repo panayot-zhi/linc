9c87b16326cfe26bc44b127535277e539c158b09
Panayot Ivanov <panayot_zhi@abv.bg>
2024-01-03 14:01:53 +0200

    small changes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,31 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+(function () {
+    "use strict";
+
+    // Bind Submit targets href form
+    $(".submit-link").on('click', doSubmit);
+    $("form").on('submit', onSubmit);
+
+    function doSubmit(e) {
+        e.preventDefault();
+
+        let target = $(e.target).attr("href");
+        target = $(target);
+        target.submit();
+
+        return false;
+    }
+
+    function onSubmit(e) {
+        let form = $(e.target);
+
+        if (form.valid()) {
+            let preloader = document.getElementById('preloader');
+            preloader.hidden = false;
+        }
+    }
+
+})()
