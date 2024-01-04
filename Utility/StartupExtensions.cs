9f31b9f287571c827ff7d5d5308ad14f825bd2b2
Panayot Ivanov <panayot_zhi@abv.bg>
2024-01-04 14:39:36 +0200

    trim this from here

--- Utility/StartupExtensions.cs
+++ Utility/StartupExtensions.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using linc.Resources;
 using Microsoft.AspNetCore.Mvc.Razor;
 
 namespace linc.Utility;
