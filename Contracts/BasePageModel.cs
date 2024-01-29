3ac57d4a49cebaf026e9659bc556b8fc7cebd6ab
Panayot Ivanov <panayot_zhi@abv.bg>
2024-01-29 11:58:12 +0200

    use base classes

---
+++ Contracts/BasePageModel.cs
@@ -0,0 +1,39 @@
+﻿using linc.Models.Enumerations;
+using linc.Utility;
+using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace linc.Contracts;
+
+public abstract class BasePageModel : PageModel
+{
+    protected readonly ILocalizationService LocalizationService;
+
+    protected BasePageModel(ILocalizationService localizationService)
+    {
+        LocalizationService = localizationService;
+    }
+
+    protected void AddAlertMessage(LocalizedHtmlString message,
+        string title = "",
+        string footer = "",
+        string position = "center",
+        string confirmButtonText = "OK",
+        AlertMessageType type = AlertMessageType.Info) =>
+        AddAlertMessage(message.Value,
+            title,
+            footer,
+            position,
+            confirmButtonText,
+            type);
+
+    protected void AddAlertMessage(string message,
+        string title = "",
+        string footer = "",
+        string position = "center",
+        string confirmButtonText = "OK",
+        AlertMessageType type = AlertMessageType.Info) =>
+        HelperFunctions.AddAlertMessage(TempData, LocalizationService,
+            message, title, footer, position, confirmButtonText, type);
+}
