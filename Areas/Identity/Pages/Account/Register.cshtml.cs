a475970e5cadb82b5dfd208f1061ed405a2a74e9
Panayot Ivanov <panayot_zhi@abv.bg>
2024-02-08 21:23:23 +0200

    this alert should be always visible

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -152,11 +152,11 @@ namespace linc.Areas.Identity.Pages.Account
                 await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                     $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
+                AddAlertMessage(LocalizationService["RegisterConfirmation_InfoMessage"],
+                    type: AlertMessageType.Success);
+
                 if (_userManager.Options.SignIn.RequireConfirmedAccount)
                 {
-                    AddAlertMessage(LocalizationService["RegisterConfirmation_InfoMessage"],
-                        type: AlertMessageType.Success);
-
                     return Redirect("/");
                 }
 
