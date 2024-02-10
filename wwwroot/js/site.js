a811c518db57619ef9ba52f047aefbd9562c63aa
Panayot Ivanov <panayot_zhi@abv.bg>
2024-02-10 17:07:37 +0200

    trigger popovers

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -58,6 +58,13 @@
     //    return false;
     //});
 
+    // Trigger popovers
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    })
+
     window.currentEditingElementId = null;
 
     window.showPreloader = function () {
