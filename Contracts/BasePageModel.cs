0c133dc5d2dc9981f42afe94ddf8e13590c50556
Panayot Ivanov <panayot_zhi@abv.bg>
2024-02-10 18:57:51 +0200

    add status message shortcuts

--- Contracts/BasePageModel.cs
+++ Contracts/BasePageModel.cs
@@ -15,6 +15,30 @@ public abstract class BasePageModel : PageModel
         LocalizationService = localizationService;
     }
 
+    protected string SuccessStatusMessage(LocalizedHtmlString message) => 
+        SuccessStatusMessage(message.Value);
+
+    protected string SuccessStatusMessage(string message)
+    {
+        return LocalizationService["StatusMessage_Success_Prefix"].Value + message;
+    }
+
+    protected string WarningStatusMessage(LocalizedHtmlString message) => 
+        WarningStatusMessage(message.Value);
+
+    protected string WarningStatusMessage(string message)
+    {
+        return LocalizationService["StatusMessage_Warning_Prefix"].Value + message;
+    }
+
+    protected string ErrorStatusMessage(LocalizedHtmlString message) => 
+        ErrorStatusMessage(message.Value);
+
+    protected string ErrorStatusMessage(string message)
+    {
+        return LocalizationService["StatusMessage_Error_Prefix"].Value + message;
+    }
+
     protected void AddAlertMessage(LocalizedHtmlString message,
         string title = "",
         string footer = "",
