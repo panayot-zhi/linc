f32e58b603f52b21ccdc88a991a0c75c21dbd634
Panayot Ivanov <panayot_zhi@abv.bg>
2024-02-25 20:09:02 +0200

    implement most needed methods

--- Contracts/IIssueService.cs
+++ Contracts/IIssueService.cs
@@ -1,9 +1,14 @@
-﻿using linc.Models.ViewModels.Issue;
+﻿using linc.Data;
+using linc.Models.ViewModels.Issue;
 
 namespace linc.Contracts
 {
     public interface IIssueService
     {
+        Task<ApplicationDocument> GetFileAsync(int id);
+
+        Task<ApplicationIssue> GetIssueAsync(int id);
+
         Task<int> CreateIssueAsync(IssueCreateViewModel input);
     }
 }
--- Services/IssueService.cs
+++ Services/IssueService.cs
@@ -6,6 +6,7 @@ using linc.Models.ViewModels.Issue;
 using linc.Utility;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using NuGet.Packaging;
 
@@ -20,6 +21,18 @@ namespace linc.Services
         {
             _context = context;
             _config = configOptions.Value;
+        }
+
+        public async Task<ApplicationDocument> GetFileAsync(int id)
+        {
+            return await _context.Documents.FindAsync(id);
+        }
+
+        public async Task<ApplicationIssue> GetIssueAsync(int id)
+        {
+            return await _context.Issues
+                .Include(x => x.Files)
+                .FirstOrDefaultAsync(x => x.Id == id);
         }
 
         public async Task<int> CreateIssueAsync(IssueCreateViewModel input)
@@ -77,14 +90,31 @@ namespace linc.Services
             int releaseYear, int issueNumber)
         {
             var fileExtension = inputFile.Extension();
-            var fileName = $"{releaseYear}-{issueNumber.ToString().PadLeft(3, '0')}";
+            var fileName = $"{releaseYear}-{issueNumber.ToString().PadLeft(3, '0')}-{HelperFunctions.ToKebabCase(type.ToString())}";
+            if (type == ApplicationDocumentType.IndexPage)
+            {
+                fileName = fileName + "-" + "1".PadLeft(2, '0');
+            }
+
             var rootFolderPath = Path.Combine(_config.RepositoryPath, SiteConstant.IssuesFolderName);
             var directoryPath = Path.Combine(rootFolderPath, releaseYear.ToString());
             var filePath = Path.Combine(directoryPath, $"{fileName}.{fileExtension}");
-            var relativePath = Path.Combine(SiteConstant.IssuesFolderName, releaseYear.ToString(),
-                $"{fileName}.{fileExtension}");
 
             Directory.CreateDirectory(directoryPath);
+            if (type == ApplicationDocumentType.IndexPage && File.Exists(filePath))
+            {
+                var number = 2;
+                while (File.Exists(filePath))
+                {
+                    fileName = fileName.Remove(fileName.Length - 2, 2);
+                    fileName = fileName + number.ToString().PadLeft(2, '0');
+                    filePath = Path.Combine(directoryPath, $"{fileName}.{fileExtension}");
+                    number++;
+                }
+            }
+
+            var relativePath = Path.Combine(SiteConstant.IssuesFolderName, releaseYear.ToString(),
+                $"{fileName}.{fileExtension}");
 
             await using (var fileStream = new FileStream(filePath, FileMode.Create))
             {
