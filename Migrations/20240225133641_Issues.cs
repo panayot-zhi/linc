e425b59b0e92417924aafbfe687252086f93761a
Panayot Ivanov <panayot_zhi@abv.bg>
2024-02-25 15:37:12 +0200

    add migration

---
+++ Migrations/20240225133641_Issues.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace linc.Migrations
+{
+    public partial class Issues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "documents",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    title = table.Column<string>(type: "varchar(256)", maxLength: 256, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    file_name = table.Column<string>(type: "varchar(127)", maxLength: 127, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    extension = table.Column<string>(type: "varchar(32)", maxLength: 32, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    mime_type = table.Column<string>(type: "varchar(128)", maxLength: 128, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    document_type = table.Column<int>(type: "int", nullable: false),
+                    relative_path = table.Column<string>(type: "varchar(512)", maxLength: 512, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_documents", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "issues",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    issue_number = table.Column<int>(type: "int", nullable: false),
+                    release_year = table.Column<int>(type: "int", nullable: false),
+                    description = table.Column<string>(type: "varchar(1024)", maxLength: 1024, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    pdf_file_id = table.Column<int>(type: "int", nullable: true),
+                    last_updated = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    date_created = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_issues", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_issues_documents_pdf_file_id",
+                        column: x => x.pdf_file_id,
+                        principalTable: "documents",
+                        principalColumn: "id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "sources",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    first_name = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    last_name = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    title = table.Column<string>(type: "varchar(512)", maxLength: 512, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    description = table.Column<string>(type: "varchar(1024)", maxLength: 1024, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    notes = table.Column<string>(type: "varchar(1024)", maxLength: 1024, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    starting_page = table.Column<int>(type: "int", nullable: false),
+                    language_id = table.Column<int>(type: "int", nullable: false),
+                    issue_id = table.Column<int>(type: "int", nullable: true),
+                    author_id = table.Column<string>(type: "varchar(127)", maxLength: 127, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    last_updated = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    date_created = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_sources", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_sources_issues_issue_id",
+                        column: x => x.issue_id,
+                        principalTable: "issues",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "fk_sources_languages_language_id",
+                        column: x => x.language_id,
+                        principalTable: "languages",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_sources_users_author_id",
+                        column: x => x.author_id,
+                        principalTable: "asp_net_users",
+                        principalColumn: "id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_issues_pdf_file_id",
+                table: "issues",
+                column: "pdf_file_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_sources_author_id",
+                table: "sources",
+                column: "author_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_sources_issue_id",
+                table: "sources",
+                column: "issue_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_sources_language_id",
+                table: "sources",
+                column: "language_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "sources");
+
+            migrationBuilder.DropTable(
+                name: "issues");
+
+            migrationBuilder.DropTable(
+                name: "documents");
+        }
+    }
+}
