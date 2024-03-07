52479cddc35eb4396d34776d8b4b9dc4a562861c
Panayot Ivanov <panayot_zhi@abv.bg>
2024-03-07 23:02:44 +0200

    add translations

--- Models/ViewModels/Issue/SourceCreateViewModel.cs
+++ Models/ViewModels/Issue/SourceCreateViewModel.cs
@@ -6,38 +6,38 @@ namespace linc.Models.ViewModels.Issue
     public class SourceCreateViewModel
     {
         [MaxLength(512, ErrorMessageResourceName = "MaxLengthAttribute_ValidationError", ErrorMessageResourceType = typeof(Resources.ValidationResource))]
-        [Display(Name = "Заглавие")]
+        [Display(Name = "SourceCreate_Title", ResourceType = typeof(Resources.SharedResource))]
         public string Title { get; set; }
 
         [MaxLength(1024, ErrorMessageResourceName = "MaxLengthAttribute_ValidationError", ErrorMessageResourceType = typeof(Resources.ValidationResource))]
-        [Display(Name = "Бележки")]
+        [Display(Name = "SourceCreate_TitleNotes", ResourceType = typeof(Resources.SharedResource))]
         public string TitleNotes { get; set; }
 
 
 
         [MaxLength(255, ErrorMessageResourceName = "MaxLengthAttribute_ValidationError", ErrorMessageResourceType = typeof(Resources.ValidationResource))]
-        [Display(Name = "Име")]
+        [Display(Name = "SourceCreate_FirstName", ResourceType = typeof(Resources.SharedResource))]
         public string FirstName { get; set; }
 
         [MaxLength(255, ErrorMessageResourceName = "MaxLengthAttribute_ValidationError", ErrorMessageResourceType = typeof(Resources.ValidationResource))]
-        [Display(Name = "Фамилия")]
+        [Display(Name = "SourceCreate_LastName", ResourceType = typeof(Resources.SharedResource))]
         public string LastName { get; set; }
 
         [MaxLength(1024, ErrorMessageResourceName = "MaxLengthAttribute_ValidationError", ErrorMessageResourceType = typeof(Resources.ValidationResource))]
-        [Display(Name = "Кратко описание")]
+        [Display(Name = "SourceCreate_AuthorNotes", ResourceType = typeof(Resources.SharedResource))]
         public string AuthorNotes { get; set; }
 
 
 
         [Required(ErrorMessageResourceName = "RequiredAttribute_ValidationError", ErrorMessageResourceType = typeof(Resources.ValidationResource))]
-        [Display(Name = "Начална")]
+        [Display(Name = "SourceCreate_StartingPage", ResourceType = typeof(Resources.SharedResource))]
         public int? StartingPage { get; set; }
 
 
-        [Display(Name = "Брой")]
+        [Display(Name = "SourceCreate_IssueId", ResourceType = typeof(Resources.SharedResource))]
         public int? IssueId { get; set; }
 
-        [Display(Name = "Брой")]
+        [Display(Name = "SourceCreate_LanguageId", ResourceType = typeof(Resources.SharedResource))]
         public int LanguageId { get; set; }
 
 
