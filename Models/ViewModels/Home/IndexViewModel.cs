a531bf2ddaf116b3041b07709cf7f7050c607af2
Panayot Ivanov <panayot_zhi@abv.bg>
2024-03-07 21:46:34 +0200

    add view models

--- Models/ViewModels/Home/IndexViewModel.cs
+++ Models/ViewModels/Home/IndexViewModel.cs
@@ -4,6 +4,10 @@
     {
         public CountsViewModel CountsViewModel { get; set; }
 
+        public PortfolioViewModel PortfolioViewModel { get; set; }
+
         public ReviewsViewModel ReviewsViewModel { get; set; }
+
+        public AuthorsViewModel AuthorsViewModel { get; set; }
     }
 }
---
+++ Models/ViewModels/Home/PortfolioViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace linc.Models.ViewModels.Home
+{
+    public class PortfolioViewModel
+    {
+        public List<int> IssueYears { get; set; }
+
+        public List<IssueViewModel> Issues { get; set; }
+    }
+
+    public class IssueViewModel
+    {
+        public int Id { get; set; }
+
+        public int IssueNumber { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Description { get; set; }
+
+        public int CoverPageId { get; set; }
+
+        public string CoverPageRelativePath { get; set; }
+
+        // public List<int> IndexPageIds { get; set; }
+    }
+}
