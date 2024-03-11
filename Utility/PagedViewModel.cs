ffc0bd96f4572e5fea939510433d013437a01b73
Panayot Ivanov <panayot_zhi@abv.bg>
2024-03-11 23:45:15 +0200

    paged view model - neat!

---
+++ Utility/PagedViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace linc.Utility
+{
+    public abstract class PagedViewModel
+    {
+        public int PageIndex { get; }
+
+        public int PageSize { get; }
+
+        public int TotalRecords { get; }
+
+        protected PagedViewModel(int totalRecords, int pageIndex, int pageSize)
+        {
+            PageSize = pageSize;
+            PageIndex = pageIndex;
+            TotalRecords = totalRecords;
+        }
+
+        public bool HasPreviousPage => (PageIndex > 1);
+
+        public int TotalPages => (int) Math.Ceiling(d: (decimal) TotalRecords / PageSize);
+
+        public bool HasNextPage => (PageIndex < TotalPages);
+    }
+}
