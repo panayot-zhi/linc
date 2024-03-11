adba7bb940bc4825829c44a2cf270c4c394334a0
Panayot Ivanov <panayot_zhi@abv.bg>
2024-03-11 23:43:51 +0200

    use alphabet constants

--- Utility/SiteConstant.cs
+++ Utility/SiteConstant.cs
@@ -30,12 +30,14 @@ public static class SiteConstant
 
     //public const string CyrillicNamePattern = "^[\\u0400-\\u04FF][\\u0400-\\u04FF-]+[\\u0400-\\u04FF]$";
 
-    public const string AllowedUserNameLatinCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    public const string AllowedUserNameCyrillicCharacters = "абвгдежзийклмнопрстуфхцчшщъьюяАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЬЮЯ";
+    public const string LatinAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public const string CyrillicAlphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЬЮЯ";
+    public static string AllowedUserNameLatinCharacters = LatinAlphabet.ToLowerInvariant() + LatinAlphabet.ToUpperInvariant();
+    public static string AllowedUserNameCyrillicCharacters = CyrillicAlphabet.ToLowerInvariant() + CyrillicAlphabet.ToUpperInvariant();
     public const string AllowedUserNameNumberCharacters = "0123456789";
     public const string AllowedUserNameSpecialCharacters = "-._@ ";
 
-    public const string AllowedUserNameCharacters = AllowedUserNameCyrillicCharacters +
+    public static string AllowedUserNameCharacters = AllowedUserNameCyrillicCharacters +
                                                     AllowedUserNameLatinCharacters +
                                                     AllowedUserNameNumberCharacters +
                                                     AllowedUserNameSpecialCharacters;
