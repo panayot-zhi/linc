af17122263bf39865203d77aa204a288aeee5182
Panayot Ivanov <panayot_zhi@abv.bg>
2024-03-31 17:41:24 +0300

    introduce base email view model

---
+++ Models/ViewModels/Emails/BaseEmailViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace linc.Models.ViewModels.Emails
+{
+    public abstract class BaseEmailViewModel
+    {
+        public string Language { get; set; } = CultureInfo.CurrentUICulture.Name;
+
+        public bool ModelPopulated { get; set; }
+
+        public string Preview { get; set; }
+
+        public EmailLink Logo { get; set; } = new();
+
+        public List<EmailLink> FooterLinks { get; set; } = new();
+
+        public bool Unsubscribe { get; set; }
+
+        public bool IsPreviewing { get; set; }
+    }
+}
