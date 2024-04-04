e66e283e970a35d9df96ddc3e2fc74112c242c84
Panayot Ivanov <panayot_zhi@abv.bg>
2024-04-04 22:15:16 +0300

    introduce document controller

---
+++ Controllers/DocumentController.cs
@@ -0,0 +1,14 @@
+﻿using linc.Contracts;
+
+namespace linc.Controllers
+{
+    public class DocumentController : BaseController
+    {
+        public DocumentController(
+            ILocalizationService localizationService) 
+            : base(localizationService)
+        {
+        }
+
+    }
+}
