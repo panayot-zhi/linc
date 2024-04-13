1e3e13e0c2d3d027930a9719c7917b135a4fa0b0
Panayot Ivanov <panayot.zhi@gmail.com>
2024-04-13 21:21:27 +0300

    add create view model

---
+++ Models/ViewModels/Dossier/DossierCreateViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace linc.Models.ViewModels.Dossier
+{
+    public class DossierCreateViewModel
+    {
+        [Required(ErrorMessageResourceName = "RequiredAttribute_ValidationError", ErrorMessageResourceType = typeof(Resources.ValidationResource))]
+        [MaxLength(1024, ErrorMessageResourceName = "MaxLengthAttribute_ValidationError", ErrorMessageResourceType = typeof(Resources.ValidationResource))]
+        [Display(Name = "DossierCreate_Title", ResourceType = typeof(Resources.SharedResource))]
+        public string Title { get; set; }
+
+        [Required(ErrorMessageResourceName = "RequiredAttribute_ValidationError", ErrorMessageResourceType = typeof(Resources.ValidationResource))]
+        [MaxLength(255, ErrorMessageResourceName = "MaxLengthAttribute_ValidationError", ErrorMessageResourceType = typeof(Resources.ValidationResource))]
+        [Display(Name = "DossierCreate_FirstName", ResourceType = typeof(Resources.SharedResource))]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessageResourceName = "RequiredAttribute_ValidationError", ErrorMessageResourceType = typeof(Resources.ValidationResource))]
+        [MaxLength(255, ErrorMessageResourceName = "MaxLengthAttribute_ValidationError", ErrorMessageResourceType = typeof(Resources.ValidationResource))]
+        [Display(Name = "DossierCreate_LastName", ResourceType = typeof(Resources.SharedResource))]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessageResourceName = "RequiredAttribute_ValidationError", ErrorMessageResourceType = typeof(Resources.ValidationResource))]
+        [MaxLength(255, ErrorMessageResourceName = "MaxLengthAttribute_ValidationError", ErrorMessageResourceType = typeof(Resources.ValidationResource))]
+        [Display(Name = "DossierCreate_Email", ResourceType = typeof(Resources.SharedResource))]
+        public string Email { get; set; }
+
+
+        [Display(Name = "DossierCreate_OriginalFile", ResourceType = typeof(Resources.SharedResource))]
+        public IFormFile OriginalFile { get; set; }
+    }
+}
