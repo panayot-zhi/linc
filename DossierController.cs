dcb62a07aabc15ed2bc37b1b61f5a93f1838336f
Panayot Ivanov <panayot.zhi@gmail.com>
2024-04-14 13:15:04 +0300

    implement at current state

--- DossierController.cs
+++ DossierController.cs
@@ -1,7 +1,5 @@
 ﻿using linc.Contracts;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using linc.Data;
 using linc.Models.Enumerations;
 using linc.Models.ViewModels.Dossier;
 using linc.Utility;
@@ -11,20 +9,16 @@ namespace linc
     [SiteAuthorize(SiteRole.Editor)]
     public class DossierController : BaseController
     {
-        private readonly ApplicationDbContext _context;
         private readonly IDossierService _dossierService;
 
         public DossierController(
-            ApplicationDbContext context,
             IDossierService dossierService,
             ILocalizationService localizationService) 
             : base(localizationService)
         {
-            _context = context;
             _dossierService = dossierService;
         }
 
-        // GET: Dossier
         public async Task<IActionResult> Index(int? page, string sort, string order)
         {
             Enum.TryParse(order, ignoreCase: true, result: out SiteSortOrder sortOrder);
@@ -32,32 +26,28 @@ namespace linc
             return View(viewModel);
         }
 
-        // GET: Dossier/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null || _context.Dossiers == null)
+            if (id is null)
             {
                 return NotFound();
             }
 
-            var applicationDossier = await _context.Dossiers
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (applicationDossier == null)
+            var viewModel = await _dossierService.GetDossierDetailsAsync(id.Value);
+            if (viewModel == null)
             {
                 return NotFound();
             }
-
-            return View(applicationDossier);
+            
+            return View(viewModel);
         }
 
-        // GET: Dossier/Create
         [SiteAuthorize(SiteRole.HeadEditor)]
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Dossier/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         [SiteAuthorize(SiteRole.HeadEditor)]
@@ -74,97 +64,47 @@ namespace linc
 
         }
 
-        // GET: Dossier/Edit/5
+        public async Task<IActionResult> AssignToMe(int? id)
+        {
+            if (id is null)
+            {
+                return NotFound();
+            }
+
+            var userId = User.GetUserId();
+            await _dossierService.AssignDossierAsync(id.Value, userId);
+
+            return RedirectToAction(nameof(Details));
+        }
+
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null || _context.Dossiers == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            var applicationDossier = await _context.Dossiers.FindAsync(id);
-            if (applicationDossier == null)
+            var viewModel = await _dossierService.GetDossierEditAsync(id.Value);
+            if (viewModel == null)
             {
                 return NotFound();
             }
-            return View(applicationDossier);
+
+            return View(viewModel);
         }
 
-        // POST: Dossier/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,FirstName,LastName,Email,Status")] ApplicationDossier applicationDossier)
+        public async Task<IActionResult> Edit(DossierEditViewModel viewModel)
         {
-            if (id != applicationDossier.Id)
+            if (!ModelState.IsValid)
             {
-                return NotFound();
+                return View(viewModel);
             }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(applicationDossier);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ApplicationDossierExists(applicationDossier.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(applicationDossier);
-        }
+            await _dossierService.UpdateDossierAsync(viewModel);
 
-        // GET: Dossier/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Dossiers == null)
-            {
-                return NotFound();
-            }
-
-            var applicationDossier = await _context.Dossiers
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (applicationDossier == null)
-            {
-                return NotFound();
-            }
-
-            return View(applicationDossier);
-        }
-
-        // POST: Dossier/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Dossiers == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.Dossiers'  is null.");
-            }
-            var applicationDossier = await _context.Dossiers.FindAsync(id);
-            if (applicationDossier != null)
-            {
-                _context.Dossiers.Remove(applicationDossier);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool ApplicationDossierExists(int id)
-        {
-          return _context.Dossiers.Any(e => e.Id == id);
+            return RedirectToAction(nameof(Details));
         }
     }
 }
