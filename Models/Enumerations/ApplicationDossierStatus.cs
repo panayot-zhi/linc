4bcaaca9721a0c6ecb264feb3d1c833a03b5bf66
Panayot Ivanov <panayot.zhi@gmail.com>
2024-04-14 11:03:39 +0300

    add resources for enum

--- Models/Enumerations/ApplicationDossierStatus.cs
+++ Models/Enumerations/ApplicationDossierStatus.cs
@@ -1,16 +1,32 @@
-﻿namespace linc.Models.Enumerations;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace linc.Models.Enumerations;
 
 public enum ApplicationDossierStatus
 {
+    [Display(Name = "DossierStatus_New", ResourceType = typeof(Resources.SharedResource))]
     New = 0,
 
+    [Display(Name = "DossierStatus_Prepared", ResourceType = typeof(Resources.SharedResource))]
     Prepared = 10,
+
+    [Display(Name = "DossierStatus_InReview", ResourceType = typeof(Resources.SharedResource))]
     InReview = 20,
+
+    [Display(Name = "DossierStatus_Reviewed", ResourceType = typeof(Resources.SharedResource))]
     Reviewed = 25,
 
+
+    [Display(Name = "DossierStatus_Accepted", ResourceType = typeof(Resources.SharedResource))]
     Accepted = 50,
+
+    [Display(Name = "DossierStatus_AcceptedWithCorrections", ResourceType = typeof(Resources.SharedResource))]
     AcceptedWithCorrections = 55,
+
+    [Display(Name = "DossierStatus_AwaitingCorrections", ResourceType = typeof(Resources.SharedResource))]
     AwaitingCorrections = 70,
 
+
+    [Display(Name = "DossierStatus_Rejected", ResourceType = typeof(Resources.SharedResource))]
     Rejected = 90
 }
