9bc25b5f3e9b929657d2093b934148f536f5f9db
Panayot Ivanov <panayot.zhi@gmail.com>
2024-04-14 20:14:16 +0300

    change status enum

--- Models/Enumerations/ApplicationDossierStatus.cs
+++ Models/Enumerations/ApplicationDossierStatus.cs
@@ -17,14 +17,15 @@ public enum ApplicationDossierStatus
     Reviewed = 25,
 
 
+    [Display(Name = "DossierStatus_AwaitingCorrections", ResourceType = typeof(Resources.SharedResource))]
+    AwaitingCorrections = 40,
+
+
     [Display(Name = "DossierStatus_Accepted", ResourceType = typeof(Resources.SharedResource))]
     Accepted = 50,
 
     [Display(Name = "DossierStatus_AcceptedWithCorrections", ResourceType = typeof(Resources.SharedResource))]
     AcceptedWithCorrections = 55,
-
-    [Display(Name = "DossierStatus_AwaitingCorrections", ResourceType = typeof(Resources.SharedResource))]
-    AwaitingCorrections = 70,
 
 
     [Display(Name = "DossierStatus_Rejected", ResourceType = typeof(Resources.SharedResource))]
