ef77937578e2955499b608586372025d2d2af68a
Panayot Ivanov <panayot.zhi@gmail.com>
2024-04-20 12:06:03 +0300

    properly guarded downloads proccess

--- Controllers/DocumentController.cs
+++ Controllers/DocumentController.cs
@@ -1,5 +1,9 @@
 ﻿using linc.Contracts;
+using linc.Models.Enumerations;
+using linc.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
 
 namespace linc.Controllers
 {
@@ -7,57 +11,39 @@ namespace linc.Controllers
     {
         private readonly IDocumentService _documentService;
         private readonly ILogger<DocumentController> _logger;
+        private readonly IDossierService _dossierService;
         private readonly ISourceService _sourceService;
         private readonly IIssueService _issueService;
 
         public DocumentController(
             IIssueService issueService,
             ISourceService sourceService,
+            IDossierService dossierService,
             IDocumentService documentService,
             ILocalizationService localizationService, 
             ILogger<DocumentController> logger) 
             : base(localizationService)
         {
-            _documentService = documentService;
             _logger = logger;
+            _documentService = documentService;
+            _dossierService = dossierService;
             _issueService = issueService;
             _sourceService = sourceService;
         }
 
-        public async Task<IActionResult> DownloadDocumentFile(int? id, bool useOriginalFileName = false)
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("issue/{issueId:int}/document/{documentId:int?}")]
+        public async Task<IActionResult> LoadIssueDocument(int issueId, int? documentId, bool download = false)
         {
-            if (id == null)
+            // NOTE: documentId can be nullable here in order to return the pdf as the default document if no documentId is passed
+            var issueDocument = await _issueService.GetIssueDocumentAsync(issueId, documentId);
+            if (issueDocument is null)
             {
                 return NotFound();
             }
 
-            var document = await _documentService.GetDocumentAsync(id.Value);
-            if (document is null)
-            {
-                return NotFound();
-            }
-
-            var result = await GetDocumentFile(document.Id);
-            if (result is null)
-            {
-                return NotFound();
-            }
-
-            result.FileDownloadName = !useOriginalFileName ? 
-                $"{document.FileName}.{document.Extension}" : 
-                document.OriginalFileName;
-
-            return result;
-        }
-
-        public async Task<IActionResult> LoadDocumentFile(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var result = await GetDocumentFile(id.Value);
+            var result = await GetDocumentFile(issueDocument.Id, download);
             if (result is null)
             {
                 return NotFound();
@@ -66,42 +52,18 @@ namespace linc.Controllers
             return result;
         }
 
-        public async Task<IActionResult> LoadIssuePdf(int? id)
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("source/{sourceId:int}/document")]
+        public async Task<IActionResult> LoadSourceDocument(int sourceId, bool download = false)
         {
-            if (id is null)
-            {
-                return NotFound();
-            }
-
-            var issue = await _issueService.GetIssueAsync(id.Value);
-            if (issue is null)
-            {
-                return NotFound();
-            }
-
-            var result = await GetDocumentFile(issue.Pdf.Id);
-            if (result is null)
-            {
-                return NotFound();
-            }
-
-            return result;
-        }
-
-        public async Task<IActionResult> LoadSourcePdf(int? id)
-        {
-            if (id is null)
-            {
-                return NotFound();
-            }
-
-            var source = await _sourceService.GetSourceAsync(id.Value);
+            var source = await _sourceService.GetSourceAsync(sourceId);
             if (source is null)
             {
                 return NotFound();
             }
 
-            var result = await GetDocumentFile(source.PdfId);
+            var result = await GetDocumentFile(source.PdfId, download);
             if (result is null)
             {
                 return NotFound();
@@ -110,7 +72,29 @@ namespace linc.Controllers
             return result;
         }
 
-        private async Task<PhysicalFileResult> GetDocumentFile(int documentId)
+        [HttpGet]
+        [SiteAuthorize(SiteRole.Editor)]
+        [Route("dossier/{dossierId:int}/document/{documentId:int}")]
+        public async Task<IActionResult> LoadDossierDocument(int dossierId, int documentId, bool download = true)
+        {
+            // NOTE: most of the time one would wish to download the dossier file so download is true by default
+
+            var dossierDocument = await _dossierService.GetDossierDocumentAsync(dossierId, documentId);
+            if (dossierDocument == null)
+            {
+                return NotFound();
+            }
+
+            var result = await GetDocumentFile(documentId, download);
+            if (result is null)
+            {
+                return NotFound();
+            }
+
+            return result;
+        }
+
+        private async Task<PhysicalFileResult> GetDocumentFile(int documentId, bool download = false)
         {
             var document = await _documentService.GetDocumentAsync(documentId);
             if (document is null)
@@ -124,12 +108,27 @@ namespace linc.Controllers
                 return null;
             }
 
-            _logger.LogInformation("Sending document file {FileName} with MimeType of {MimeType} and type {DocumentType}",
-                document.FileName, document.MimeType, document.DocumentType.ToString());
+            PhysicalFileResult result;
 
-            // don't append FileDownloadName since we do not want to force
+            // don't append FileDownloadName by default since we do not want to force
             // browsers to download the file, but instead attempt to display it
-            return new PhysicalFileResult(path, document.MimeType);
+
+            if (download)
+            {
+                result = new PhysicalFileResult(path, MediaTypeNames.Application.Octet)
+                {
+                    FileDownloadName = document.OriginalFileName
+                };
+            }
+            else
+            {
+                result = new PhysicalFileResult(path, document.MimeType);
+            }
+
+            _logger.LogInformation("Sending document file {Path} with MimeType of {MimeType} and type {DocumentType} for download: {Download}",
+                path, document.MimeType, document.DocumentType.ToString(), download);
+
+            return result;
         }
     }
 }
