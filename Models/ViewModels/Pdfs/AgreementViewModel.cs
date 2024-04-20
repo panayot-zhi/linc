27339816446ecc868b0db4dc9a86b3276e190576
Panayot Ivanov <panayot.zhi@gmail.com>
2024-04-20 19:01:41 +0300

    add view models for pdfs

---
+++ Models/ViewModels/Pdfs/AgreementViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace linc.Models.ViewModels.Pdfs
+{
+    public class AgreementViewModel : BasePdfViewModel
+    {
+    }
+}
---
+++ Models/ViewModels/Pdfs/BasePdfViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace linc.Models.ViewModels.Pdfs
+{
+    public abstract class BasePdfViewModel
+    {
+        public bool IsPreviewing { get; set; }
+
+        public string Language { get; set; } = CultureInfo.CurrentUICulture.Name;
+
+        public LinkViewModel Logo { get; set; } = new();
+    }
+}
