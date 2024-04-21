2f74c218aa674bb497d214e0f2eeb8aea93c1b55
Panayot Ivanov <panayot.zhi@gmail.com>
2024-04-21 10:09:17 +0300

    bind manuscript to author

---
+++ Migrations/20240420215129_DossierAuthorAssignment.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace linc.Migrations
+{
+    public partial class DossierAuthorAssignment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "author_id",
+                table: "dossiers",
+                type: "varchar(255)",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_dossiers_author_id",
+                table: "dossiers",
+                column: "author_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_dossiers_users_author_id",
+                table: "dossiers",
+                column: "author_id",
+                principalTable: "asp_net_users",
+                principalColumn: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_dossiers_users_author_id",
+                table: "dossiers");
+
+            migrationBuilder.DropIndex(
+                name: "ix_dossiers_author_id",
+                table: "dossiers");
+
+            migrationBuilder.DropColumn(
+                name: "author_id",
+                table: "dossiers");
+        }
+    }
+}
