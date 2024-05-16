a036d27aa878d42ba5d1c868210a99a1814c4c78
Panayot Ivanov <panayot.zhi@gmail.com>
2024-05-16 22:40:11 +0300

    add source flags

---
+++ Migrations/20240512101250_AddSourceFlags.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace linc.Migrations
+{
+    public partial class AddSourceFlags : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "issue_theme",
+                table: "sources",
+                newName: "is_theme");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_section",
+                table: "sources",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_section",
+                table: "sources");
+
+            migrationBuilder.RenameColumn(
+                name: "is_theme",
+                table: "sources",
+                newName: "issue_theme");
+        }
+    }
+}
