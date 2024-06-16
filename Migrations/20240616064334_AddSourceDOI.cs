377126827afff6349b532b03de9de84831588a11
Panayot Ivanov <panayot.zhi@gmail.com>
2024-06-16 09:44:36 +0300

    add sources migration

---
+++ Migrations/20240616064334_AddSourceDOI.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace linc.Migrations
+{
+    public partial class AddSourceDOI : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "doi",
+                table: "sources",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "doi",
+                table: "sources");
+        }
+    }
+}
