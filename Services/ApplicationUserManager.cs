4d2aeba78f9a64fc1c3599fd843bfac9f4ce4f95
Panayot Ivanov <panayot.zhi@gmail.com>
2024-07-28 18:12:41 +0300

    wrap in custom class

---
+++ Services/ApplicationUserManager.cs
@@ -0,0 +1,25 @@
+﻿using linc.Contracts;
+using linc.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+namespace linc.Services;
+
+public class ApplicationUserManager : UserManager<ApplicationUser>
+{
+    private readonly IApplicationUserStore _applicationUserStore;
+
+    public ApplicationUserManager(IApplicationUserStore store, IOptions<IdentityOptions> optionsAccessor, IPasswordHasher<ApplicationUser> passwordHasher, IEnumerable<IUserValidator<ApplicationUser>> userValidators, IEnumerable<IPasswordValidator<ApplicationUser>> passwordValidators, ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors, IServiceProvider services, ILogger<UserManager<ApplicationUser>> logger) : base(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger)
+    {
+        _applicationUserStore = store;
+    }
+
+    public virtual async Task<IdentityResult> UpdateUserProfiles(ApplicationUser user, ApplicationUserProfile[] profiles)
+    {
+        ThrowIfDisposed();
+        ArgumentNullException.ThrowIfNull(user);
+        await _applicationUserStore.UpdateUserProfiles(user, profiles);
+        await UpdateSecurityStampAsync(user);
+        return await UpdateUserAsync(user);
+    }
+}
