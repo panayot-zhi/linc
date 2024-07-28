7eba6c08684d30b3642cc1209b6999f6aa96aba4
Panayot Ivanov <panayot.zhi@gmail.com>
2024-07-28 18:11:30 +0300

    my own user store, with blackjack and hookers

---
+++ Contracts/IApplicationUserStore.cs
@@ -0,0 +1,10 @@
+﻿using linc.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace linc.Contracts
+{
+    public interface IApplicationUserStore : IUserStore<ApplicationUser>
+    {
+        Task UpdateUserProfiles(ApplicationUser user, ApplicationUserProfile[] profiles);
+    }
+}
