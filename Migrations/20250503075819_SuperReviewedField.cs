e9d9ca44b70be2b374a1a9315cd5fe78ca771409
Panayot Ivanov <panayot.zhi@gmail.com>
2025-05-03 10:59:11 +0300

    add migration for super reviewed field

---
+++ Migrations/20250503075819_SuperReviewedField.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace linc.Migrations
+{
+    public partial class SuperReviewedField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "super_reviewed",
+                table: "dossiers",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "super_reviewed",
+                table: "dossiers");
+        }
+    }
+}
