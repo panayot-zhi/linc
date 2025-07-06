ca7bfe0f1f07a120fe4354dc854c5468c84d7e83
Panayot Ivanov <panayot.zhi@gmail.com>
2025-07-06 18:28:51 +0300

    link sources and dossiers by title and names

---
+++ Migrations/20250706144207_LinkSourceDossiers.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace linc.Migrations
+{
+    public partial class LinkSourceDossiers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE sources
+                JOIN dossiers d
+                ON LOWER(d.title) = LOWER(sources.title)
+                   AND LOWER(d.first_name) = LOWER(sources.first_name)
+                   AND LOWER(d.last_name) = LOWER(sources.last_name)
+                SET sources.dossier_id = d.id
+                WHERE sources.id > 0;
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE sources
+                SET dossier_id = NULL;
+            ");
+        }
+    }
+}
