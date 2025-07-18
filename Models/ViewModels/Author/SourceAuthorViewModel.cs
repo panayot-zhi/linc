c9185774ec5f2d54555e908739d700f3fe963866
Panayot Ivanov <panayot.zhi@gmail.com>
2025-07-18 18:44:23 +0300

    source author view model

---
+++ Models/ViewModels/Author/SourceAuthorViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace linc.Models.ViewModels.Author
+{
+    public class SourceAuthorViewModel
+    {
+        [Required(ErrorMessageResourceName = "RequiredAttribute_ValidationError", ErrorMessageResourceType = typeof(Resources.ValidationResource))]
+        [MaxLength(255, ErrorMessageResourceName = "MaxLengthAttribute_ValidationError", ErrorMessageResourceType = typeof(Resources.ValidationResource))]
+        [Display(Name = "SourceAuthor_FirstName", ResourceType = typeof(Resources.SharedResource))]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessageResourceName = "RequiredAttribute_ValidationError", ErrorMessageResourceType = typeof(Resources.ValidationResource))]
+        [MaxLength(255, ErrorMessageResourceName = "MaxLengthAttribute_ValidationError", ErrorMessageResourceType = typeof(Resources.ValidationResource))]
+        [Display(Name = "SourceAuthor_LastName", ResourceType = typeof(Resources.SharedResource))]
+        public string LastName { get; set; }
+
+
+        [EmailAddress(ErrorMessageResourceName = "EmailAddressAttribute_Invalid", ErrorMessageResourceType = typeof(Resources.ValidationResource))]
+        [MaxLength(255, ErrorMessageResourceName = "MaxLengthAttribute_ValidationError", ErrorMessageResourceType = typeof(Resources.ValidationResource))]
+        [Display(Name = "SourceAuthor_Email", ResourceType = typeof(Resources.SharedResource))]
+        public string Email { get; set; }
+
+
+        [MaxLength(127)]
+        public string UserId { get; set; }
+
+        [Display(Name = "SourceAuthor_UserName", ResourceType = typeof(Resources.SharedResource))]
+        public string UserName { get; set; }
+    }
+}
