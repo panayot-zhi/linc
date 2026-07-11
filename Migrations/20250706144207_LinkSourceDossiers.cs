5b8b4e987e13dde0129804eb3da40c1ac243e149
Panayot Ivanov <panayot.zhi@gmail.com>
2026-07-11 20:23:41 +0300

    filter also by language_id

--- Migrations/20250706144207_LinkSourceDossiers.cs
+++ Migrations/20250706144207_LinkSourceDossiers.cs
@@ -12,8 +12,9 @@ namespace linc.Migrations
                 UPDATE sources
                 JOIN dossiers d
                 ON LOWER(d.title) = LOWER(sources.title)
-                   AND LOWER(d.first_name) = LOWER(sources.first_name)
-                   AND LOWER(d.last_name) = LOWER(sources.last_name)
+                    AND LOWER(d.first_name) = LOWER(sources.first_name)
+                    AND LOWER(d.last_name) = LOWER(sources.last_name)
+                    AND d.language_id = sources.language_id
                 SET sources.dossier_id = d.id
                 WHERE sources.id > 0;
             ");
